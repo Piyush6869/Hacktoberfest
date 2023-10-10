d69b04918fb641f0b1ec73b7c135e8528cbb5d9f
triabagus <triatop9@gmail.com>
2023-10-10 14:53:01 +0700

    first commit

--- new.php
+++ new.php
@@ -1,6 +1,5 @@
 <?php  
-                echo"<h1>Hello</h1>";
-                echo"Trying to get something fro this hacktoberfest";
-                echo"my first repo :)";
-                
+    echo"<h1>Hello</h1>";
+    echo"Trying to get something fro this hacktoberfest";
+    echo"my first repo :)";
 ?>
